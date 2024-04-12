c77b8ea16d1e774128d7e6eeaf9b0a8eb83cf890
Ihr Name auf github <hl5162@mci4me.at>
2024-04-12 13:19:41 +0200

    Zeitachse in Minuten erstellt

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ from save_path import save_path
 data = load_data('activity.csv')
 sorted_power_W = bubbleSort(data['PowerOriginal'])
 
-# Plot the data
-plt.plot(sorted_power_W[::-1])
+# Zeitachse erstellen --> 1804 Einträge bei 30:05 min
+datalength = range(len(sorted_power_W)) # ermittelt die Länge der Daten
+minutes = [i/60 for i in datalength] # ermittelt die Zeit in Minuten
 
-plt.xlabel('Time/s')
+# Plot the data
+plt.plot(minutes, sorted_power_W[::-1])
+
+plt.xlabel('Time/min')
 plt.ylabel('Power/W')
-plt.title('Power over Time')
+plt.title('Leistungskurve')
 
 #plt.savefig(r'figures/power_over_time.png') quick and dirty. also os independent
 plt.savefig(save_path("figures", "power_over_time.png"))
