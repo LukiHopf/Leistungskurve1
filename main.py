c91f1bed9ce1dcd899e053c1cd40e769c3bdcc63
Ihr Name auf github <hl5162@mci4me.at>
2024-04-12 13:08:38 +0200

    klnw

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sort import bubbleSort
 
 # Anzahl der Einträge 1804 bei 30:05 min
 
+# Daten für Plot laden und sortieren
 data = load_data('activity.csv')
 sorted_power_W = bubbleSort(data['PowerOriginal'])
 
